57319965c2c7c7aef43ea0bff95b9e8f16c8c630
Mickle <putilovms@gmail.com>
2024-03-17 17:31:19 +0400

    Linter error correction

--- brain_games/games/controller.py
+++ brain_games/games/controller.py
@@ -29,7 +29,8 @@ def print_rule_game(game):
         case 'progression':
             print('What number is missing in the progression?')
         case 'prime':
-            print('Answer "yes" if given number is prime. Otherwise answer "no".')
+            print('Answer "yes" if given number is prime.' +
+                  + 'Otherwise answer "no".')
 
 
 def qestions_game(game):
