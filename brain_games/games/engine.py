c246e4ed5eaa029642296102550f98669eb212a8
Mickle <putilovms@gmail.com>
2024-03-17 14:27:11 +0400

    Modify engine.py

--- brain_games/games/engine.py
+++ brain_games/games/engine.py
@@ -59,6 +59,29 @@ def qestions_game_gcd():
     return False
 
 
+# Задать вопрос для игры progression
+# Возвращает результат ответа
+def qestions_game_progression():
+    PROG_LEN = 10
+    prog = prog_gen(PROG_LEN, randint(1, 10), randint(1, 20))
+    hide_number = randint(0, PROG_LEN - 1)
+    correct_answer = prog[hide_number]
+    prog[hide_number] = '..'
+    answer = prompt.string('Question: ' + ' '.join(map(str, prog)) + ' ')
+    print_answer(answer)
+    if (answer == str(correct_answer)):
+        return True
+    hint_not_correct(answer, correct_answer)
+    return False
+
+
+# Генерирует арифметическую прогрессию длинной lenght,
+# с шагом step, начиная с числа start
+def prog_gen(lenght, step, start):
+    progression = [(i * step) + start for i in range(lenght)]
+    return (progression)
+
+
 # Поиск максимального делителя двух чисел
 # по алгоритму Евклида
 def getGcd(a, b):
